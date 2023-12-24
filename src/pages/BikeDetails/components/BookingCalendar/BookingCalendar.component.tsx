a1e43ec4b31e12bb456d1bfdadb23577c97313af
Alessandro Rodrigo <alessandro.fresneda84@gmail.com>
2023-12-23 23:07:59 -0300

    Fix disabled dates in BookingCalendar.component.tsx

--- src/pages/BikeDetails/components/BookingCalendar/BookingCalendar.component.tsx
+++ src/pages/BikeDetails/components/BookingCalendar/BookingCalendar.component.tsx
@@ -107,7 +107,10 @@ function CalendarBody() {
             isSelected={isSelected(day)}
             isTail={!!selectedDays.start && BookingCalendarUtils.isSameDay(day, selectedDays.start)}
             isHead={!!selectedDays.end && BookingCalendarUtils.isSameDay(day, selectedDays.end)}
-            disabled={BookingCalendarUtils.isBefore(day)}
+            disabled={
+              BookingCalendarUtils.isBefore(day) ||
+              !BookingCalendarUtils.isSameMonth(day, currentDate)
+            }
             key={index}
           >
             {day.getDate()}
